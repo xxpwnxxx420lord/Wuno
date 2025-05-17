3fce324f3bbc014ae2f8d4ecc0c3e2fdff898b69
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2025-05-17 14:18:41 +1000

    Create Services.lua

---
+++ Modules/Services.lua
@@ -0,0 +1,11 @@
+Services = {
+    P = game:GetService("Players"),
+    RS = game:GetService("ReplicatedStorage"),
+    WS = game:GetService("Workspace"),
+    SS = game:GetService("SoundService"),
+    UIS = game:GetService("UserInputService"),
+    TS = game:GetService("TweenService"), -- icl ts pmo 
+    HS = game:GetService("HttpService"),
+}
+
+return Services
