cd5601fcb6f0501437a76bc339b247550b916ef9
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2025-05-17 16:26:08 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,17 @@
-local base_url = "https://raw.githubusercontent.com/xxpwnxxx420lord/Wuno/games/"
-local url = base_url .. tostring(game.PlaceId)
+--[[
+    If you do 'steal' this starting script please give me credits as I spent 5 minutes of my day working on this <3333333      
+]]
+local base_url = "https://raw.githubusercontent.com/xxpwnxxx420lord/Wuno/refs/heads/main/games/"
+local url = base_url .. tostring(game.PlaceId)..".lua"
 local success, response = pcall(function()
     return game:HttpGet(url)
 end)
+
 if success and response and response ~= "" then
     local func, err = loadstring(response)
     if func then
-        local ok, result = pcall(func)
-        if not ok then
-            warn("Error running the loaded script: ".. tostring(result))
-        end
+        func()  -- work NOW! 
     else
-        warn("Failed to load script: " .. tostring(err))
+        game.Players.LocalPlayer:Kick("Currently, there is no universal script available. If you encounter a bug, please visit our support channel at Discord (copied in your clipboard) for assistance with supported games. The link has been copied to your clipboard for your convenience.")
     end
-else
-    warn("Failed to fetch script from URL or empty response")
 end
