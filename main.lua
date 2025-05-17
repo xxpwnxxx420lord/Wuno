bd1d5483ace8eb7e10d5a714b556ad559dff25e4
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2025-05-17 16:11:22 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,18 @@
-
+local base_url = "https://raw.githubusercontent.com/xxpwnxxx420lord/Wuno/games/"
+local url = base_url .. tostring(game.PlaceId)
+local success, response = pcall(function()
+    return game:HttpGet(url)
+end)
+if success and response and response ~= "" then
+    local func, err = loadstring(response)
+    if func then
+        local ok, result = pcall(func)
+        if not ok then
+            warn("Error running the loaded script: ".. tostring(result))
+        end
+    else
+        warn("Failed to load script: " .. tostring(err))
+    end
+else
+    warn("Failed to fetch script from URL or empty response")
+end
